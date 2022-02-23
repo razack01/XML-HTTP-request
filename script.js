6e929a41a9a722680d90553a1b998fa119ff91d9
Abdul Razack AR <75486001+razack01@users.noreply.github.com>
2022-02-23 19:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// create an xhr object
+var request=new XMLHttpRequest();
+// request a connection(which data you require?)
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+// sending a connection request
+request.send();
+// once the data successfully received
+// from the server 200
+request.onload=function(){
+	var data=JSON.parse(request.response);
+	console.log(data);
+	
+
+	for(var i=0; i<10;i++){
+		console.log(`flag:${data[i].flag}`)
+	}
+// 	
+
+
